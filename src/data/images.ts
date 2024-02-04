1ab85edbde0ab57495afa33d22e333537f17ec35
aort <artashesano@gmail.com>
2024-02-04 13:00:26 +0400

    Update images

--- src/data/images.ts
+++ src/data/images.ts
@@ -1,5 +1,5 @@
 const numbers = Array.from(Array(6), (_, index) => (index += 1));
 
-const images = numbers.map((value) => `/${value}.png`);
+const images = numbers.map((value) => `/${value}.jpeg`);
 
 export { images };
